e0854b65b46d92b6bb5604ee38bf04f2baacbfc5
Shankar Siddharth K A <shankarsiddharthka@gmail.com>
2017-07-07 05:18:28 +0530

    code formatting

--- db/db.js
+++ db/db.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const connectionString = "mongodb://localhost/polldb";
-mongoose.connect(connectionString, {useMongoClient: true});
+mongoose.connect(connectionString, { useMongoClient: true });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.info('connection established successfully with mongodb')
+db.on('error', console.error.bind(console, 'connection error: Please, check the mongodb server is running in localhost port 27017. Otherwise, please change the connection string accordingly in the file db/db.js'));
+db.once('open', function () {
+  console.info('Connection established successfully with mongodb');
+  console.info("The mongodb port is 27017");
+  console.info("The new database name would be 'polldb' ");
 });
 
 module.exports.mongoose = mongoose;
